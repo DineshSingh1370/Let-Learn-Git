34b1f51bce274c46562c083a218ab67f4df1b312
Dinesh Singh <2018ume1370@mnit.ac.in>
2022-12-04 14:19:59 +0530

    in q

---
+++ q.py
@@ -0,0 +1,19 @@
+a = int(input('enter first number '))
+b = int(input('enter second number '))
+
+def add():
+    return a+b
+
+def sub():
+    return a-b
+
+def mul():
+    return a*b
+
+def div():
+    return a/b
+
+print(add())
+print(sub())
+print(mul())
+print(div())
